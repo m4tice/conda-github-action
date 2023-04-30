0b714bac92f7311fb28d2e90dd9e8755a0d13956
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-05-01 01:26:18 +0700

    Update action and fastapi App

--- dummy.py
+++ dummy.py
@@ -1,7 +1,25 @@
 """dummy"""
 
+
+from fastapi import FastAPI
+
+
 def addition(first_number: int, second_number: int):
     """
     addtion of first number and second number
     """
     return first_number + second_number
+
+
+
+DEVICES = ["device_01", "device_02", "device_03"]
+
+app = FastAPI()
+
+
+@app.get("/devices")
+async def get_devices():
+    """
+    get all devices
+    """
+    return DEVICES
