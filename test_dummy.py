6e2cd1a9c2dfbcc3d0cc52448a5f1ed153c8e996
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-05-01 01:55:14 +0700

    Add test for get devices

--- test_dummy.py
+++ test_dummy.py
@@ -1,6 +1,6 @@
 """Test dummy"""
 
-
+from fastapi.testclient import TestClient
 import dummy
 
 
@@ -9,3 +9,15 @@ def test_addition():
     Test dummy::addition
     """
     assert dummy.addition(1, 2) == 3
+
+app = dummy.app()
+client = TestClient(app)
+
+
+def test_get_device():
+    """
+    test get devices
+    """
+    response = client.get("/devices")
+    assert response.status_code == 200
+    assert response.json() == ["device_01","device_02","device_03"]
