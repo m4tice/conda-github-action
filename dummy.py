0b94427f1775b6d61be46235b59f24c1c4712c95
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-04-30 15:18:57 +0700

    Update test and action

---
+++ dummy.py
@@ -0,0 +1,7 @@
+"""dummy"""
+
+def addition(first_number: int, second_number: int):
+    """
+    addtion of first number and second number
+    """
+    return first_number + second_number
---
+++ test_dummy.py
@@ -0,0 +1,11 @@
+"""Test dummy"""
+
+
+import dummy
+
+
+def test_addition():
+    """
+    Test dummy::addition
+    """
+    assert dummy.addition(1, 2) == 3
